eeeab3c6d2ca3d63f0d82ce9265d14031ea33b21
Altana <altanagrishkina@gmail.com>
2023-07-10 15:50:16 +0200

    Using Watchers

--- vue-s-spa/src/components/CreatePage.vue
+++ vue-s-spa/src/components/CreatePage.vue
@@ -86,6 +86,13 @@ export default {
             this.linkUrl = '';
             this.published = true
         }
+    },
+    watch: {
+        pageTitle(newTitle, oldTitle) {
+            if (this.linkText === oldTitle) {
+                this.linkText = newTitle;
+            }
+        }
     }
 }
 </script>
