77b7d1b9e84aca5728a1b8f325ff9702560db1f0
Altana <altanagrishkina@gmail.com>
2023-07-12 15:59:51 +0200

    Nesting Routes

--- vue-s-spa/src/App.vue
+++ vue-s-spa/src/App.vue
@@ -2,24 +2,14 @@
     <navbar></navbar>
 
         <router-view></router-view>
-    <!-- <page-viewer 
-        v-if="pages.length > 0" 
-        :page="pages[activePage]"></page-viewer>
-
-    <div v-show="false">hide this content</div>
-    <create-page @page-created="pageCreated"></create-page> -->
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
-import PageViewer from './components/PageViewer.vue';
-import CreatePage from './components/CreatePage.vue';
 
 export default ({
     components: {
-        PageViewer,
-        Navbar,
-        CreatePage
+        Navbar
     },
 
     methods: {
