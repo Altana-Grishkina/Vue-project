eef98feed801a1e7ce7c1696b982593145217df5
Altana <altanagrishkina@gmail.com>
2023-07-12 18:32:46 +0200

    Introducing the Composition API

--- vue-s-spa/src/views/PagesList.vue
+++ vue-s-spa/src/views/PagesList.vue
@@ -1,3 +1,31 @@
 <template>
-    <p>this is the pages list</p>
+    <p>{{counter }}</p>
+    <button @click.prevent="data.counter++">click</button>
 </template>
+
+<script setup>
+import {ref, reactive} from 'vue';
+
+// const counter = ref(0);
+const data = reactive({counter: 0})
+
+function click() {
+    // counter.value++;
+    // console.log(counter);
+    data.counter++;
+}
+
+// export default {
+//     data() {
+//         return {
+//             counter: 0
+//         };
+//     },
+//     methods: {
+//         click() {
+//             this.counter++;
+//         }
+//     }
+// }
+
+</script>
